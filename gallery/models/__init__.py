1d0b6467a3138f39fdcb86dfd90bedae830a7ded
Madi <madi_gaziz@mail.ru>
2023-09-30 11:45:31 +0600

    Создал модель Photos

---
+++ gallery/models/__init__.py
@@ -0,0 +1,1 @@
+from gallery.models.photos import Photo
---
+++ gallery/models/photos.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+
+class Photo(models.Model):
+    image = models.ImageField(upload_to='photos', null=False, blank=False)
+    signature = models.CharField(max_length=250, null=False, blank=False,)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Created At')
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='User', null=False, blank=False)
